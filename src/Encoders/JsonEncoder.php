a76db40cbe290e5d2afee17c6271ae6ffc269cfb
ueef <artem@arkadev.org>
2019-01-25 12:07:48 +0500

    catch json encoding errors

--- src/Encoders/JsonEncoder.php
+++ src/Encoders/JsonEncoder.php
@@ -7,7 +7,6 @@ namespace Ueef\Encoder\Encoders {
     use Ueef\Encoder\Interfaces\EncoderInterface;
 
 
-
     class JsonEncoder implements EncoderInterface
     {
         /** @var integer */
@@ -21,14 +20,21 @@ namespace Ueef\Encoder\Encoders {
 
         public function encode(array $message): string
         {
-            return json_encode($message, $this->encode_options);
+            $message = json_encode($message, $this->encode_options);
+
+            if (JSON_ERROR_NONE !== json_last_error()) {
+                throw new EncoderException('Json encoding error: ' . json_last_error_msg(), json_last_error());
+            }
+
+            return $message;
         }
+
         public function decode(string $message): array
         {
             $message = json_decode($message, true);
 
             if (JSON_ERROR_NONE !== json_last_error()) {
-                throw new EncoderException('Json error: ' . json_last_error_msg(), json_last_error());
+                throw new EncoderException('Json decoding error: ' . json_last_error_msg(), json_last_error());
             }
 
             return $message;
